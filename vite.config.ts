92a6d285508b7aa7fe52cd8b13c3bfad32d6c5bd
teemoZipsa <teemoZipsa@users.noreply.github.com>
2026-03-31 22:59:52 +0900

    chore: 프로젝트 이사(119helper.github.io)에 따른 도메인 라우팅 변경

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/119-helper/' : '/',
+  base: '/',
   plugins: [react()],
   server: {
     proxy: {
